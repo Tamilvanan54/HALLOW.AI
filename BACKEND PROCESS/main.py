f7eb6771bff65c107bf1205d445151ed5faa0b26
Tamilvanan <stamilvanan54@gmail.com>
2026-08-17 15:10:49 +0530

    Allow all origins wildcard in BACKEND PROCESS main.py to fix CORS upload blocks on hosted server

--- BACKEND PROCESS/main.py
+++ BACKEND PROCESS/main.py
@@ -87,35 +87,11 @@ class ChatRequest(BaseModel):
 
 
 app.add_middleware(
-
     CORSMiddleware,
-
-
-    allow_origins=[
-
-
-        "http://localhost:5173",
-
-        "http://localhost:5174",
-
-
-        "http://127.0.0.1:5173",
-
-        "http://127.0.0.1:5174"
-
-
-    ],
-
-
+    allow_origins=["*"],
     allow_credentials=True,
-
-
     allow_methods=["*"],
-
-
-    allow_headers=["*"]
-
-
+    allow_headers=["*"],
 )
 
 
